89cda1c1d9a0743af5d52a06f5bfcbb55e6d08bf
rajasekhar <you@example.com>
2018-11-25 07:00:24 +0530

    #1 first question added

---
+++ CodingTest/src/com/blogspot/javabyrajasekhar/Test.java
@@ -0,0 +1,20 @@
+package com.blogspot.javabyrajasekhar;
+
+public class Test {
+	public static void main(String[] args) {
+		A a = new A();
+		a.nullcheck(null);
+	}
+}
+
+class A {
+
+	public void nullcheck(Object o) {
+		System.out.println("i am from object");
+	}
+
+	public void nullcheck(String s) {
+		System.out.println("i am from string");
+	}
+
+}
